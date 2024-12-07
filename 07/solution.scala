dc76a44d444edbafba161b3499360639dbc4e71b
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2024-12-07 22:51:52 +0100

    Format day 7

--- 07/solution.scala
+++ 07/solution.scala
@@ -12,29 +12,41 @@ type Input = Seq[(Long, Seq[Long])]
 type Output = Long
 
 def parseInput(input: String): Input =
-  input.split('\n').map(line =>
-    val Array(testValue, rest) = line.split(": ")
-    (testValue.toLong, rest.split(' ').map(_.toLong).toSeq)
-  ).toSeq
+  input
+    .split('\n')
+    .map(line =>
+      val Array(testValue, rest) = line.split(": ")
+      (testValue.toLong, rest.split(' ').map(_.toLong).toSeq)
+    )
+    .toSeq
 
 def possibleResults(numbers: List[Long]): Set[Long] = numbers match
   case x :: Nil => Set(x)
-  case x :: xs => possibleResults(xs).map(x + _) ++ possibleResults(xs).map(x * _)
+  case x :: xs =>
+    possibleResults(xs).map(x + _) ++ possibleResults(xs).map(x * _)
 
 def part1(input: Input): Output =
-  input.filter((testValue, numbers) =>
+  input
+    .filter((testValue, numbers) =>
       possibleResults(numbers.toList.reverse).contains(testValue)
-  ).map(_._1).sum
+    )
+    .map(_._1)
+    .sum
 
 def concat(a: Long, b: Long): Long =
   s"$b$a".toLong
-def possibleResults2(numbers: List[Long]): Set[Long] = 
-  val operators = Seq((x: Long, y: Long) => x+y, (x: Long,y: Long) => x*y, concat)
+def possibleResults2(numbers: List[Long]): Set[Long] =
+  val operators =
+    Seq((x: Long, y: Long) => x + y, (x: Long, y: Long) => x * y, concat)
   numbers match
     case x :: Nil => Set(x)
-    case x :: xs => operators.flatMap(op => possibleResults2(xs).map(y => op(x,y))).toSet
+    case x :: xs =>
+      operators.flatMap(op => possibleResults2(xs).map(y => op(x, y))).toSet
 
 def part2(input: Input): Output =
-  input.filter((testValue, numbers) =>
+  input
+    .filter((testValue, numbers) =>
       possibleResults2(numbers.toList.reverse).contains(testValue)
-  ).map(_._1).sum
+    )
+    .map(_._1)
+    .sum
